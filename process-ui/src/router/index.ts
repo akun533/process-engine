ee9f2aa40f6853cf50b8e934b560de47a56a5817
akun <122915623@qq.com>
2025-12-22 17:53:10 +0800

    refactor(router): 优化路由类型导入方式
    - 将 RouteRecordRaw 类型导入方式改为显式 type 导入
    - 保持 vue-router 的其他功能导入不变
    - 提升代码类型安全性与可读性

--- process-ui/src/router/index.ts
+++ process-ui/src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 import ProcessManagement from '../views/ProcessManagement.vue';
 import ProcessDesigner from '../views/ProcessDesigner.vue';
 
